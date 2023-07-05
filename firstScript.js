cbe838dbce0c924b00685220d873d374f59d5e75
DeepakDua-appinventiv <Deepak.Dua@appinventiv.com>
2023-07-05 12:07:36 +0530

    files commit

---
+++ firstScript.js
@@ -0,0 +1,28 @@
+// for(let i=0; i<10; i++){
+//     console.log("Hello There!");
+// }
+
+// let str = "Hello";
+// if(str == "Hello"){
+//     console.log("string matched");
+// }
+
+
+//Array Methods Practice
+
+// let arr1 = ['a', 'b', 'c'];
+// let arr2 = ['d', 'e', 'f'];
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// let arr1 = ['cat', 'dog', 'bat'];
+// console.log(arr1.includes('cat'));
+
+// let arr1 = ['sun', 'moon', 'stars'];
+// console.log(arr1.reverse());
+
+// let colors = ['red', 'green', 'yellow', 'purple', 'orange', 'golden'];
+// console.log(colors.slice(2,4));
+
+let colors = ['red', 'green', 'yellow', 'purple', 'orange', 'golden'];
+console.log(colors.splice(2,1,'pink'));
